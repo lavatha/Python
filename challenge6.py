920181d3c5e526eb3769fe54cd6c4baec8cef92d
Lavatharini <134617779+lavathariny@users.noreply.github.com>
2025-01-23 09:30:59 -0500

    challenge6.py

--- challenge6.py
+++ challenge6.py
@@ -8,21 +8,21 @@ import sys
 
 def find_last_letter(line):
     closest_to_z = None # Initialize variable to store the closest letter to 'z'
-    for char in line.lower():  # Iterate through each character in the line
+    for char in line.lower(): 
         if char.isalpha(): # Check if the character is an alphabet
             if closest_to_z is None or char > closest_to_z: # If closest_to_z is None or the current character is greater than the closest_to_z
-                closest_to_z = char # Update closest_to_z with the current character
+                closest_to_z = char 
                 
-    return closest_to_z    # Return the closest letter to 'z'
+    return closest_to_z    
 
 if len(sys.argv) != 2: # Check if the number of command line arguments is not equal to 2
-    print("Usage: python challenge6.py <filename>") # Print usage message
+    print("Usage: python challenge6.py <filename>") 
 else:
     filename = sys.argv[1] # Get the filename
     try:
         with open(filename, 'r') as file:
-            for line in file: # Iterate through each line in the file
-                last_letter = find_last_letter(line) # Find the last letter in the line
+            for line in file: 
+                last_letter = find_last_letter(line) 
                 if last_letter:
                     print(last_letter) # If last_letter is not None, print it
 
